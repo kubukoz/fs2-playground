6f89957e469026dd4a5630c624d9e287558f8a03
Jakub Kozłowski <kubukoz@gmail.com>
2018-09-24 12:01:46 +0200

    Add a reproducible for hanging queues

--- src/main/scala/io/typelevel/Playground.scala
+++ src/main/scala/io/typelevel/Playground.scala
@@ -1,11 +1,38 @@
 package io.typelevel
 import cats.effect._
 import cats.implicits._
+import fs2.Stream
+import fs2.concurrent.InspectableQueue
+
+import scala.language.higherKinds
 
 object Playground extends IOApp {
 
+  private def putStrLn[F[_]: Sync](msg: String): F[Unit] = {
+    Sync[F].delay(println(msg))
+  }
+
+  val hangingQueue: IO[Unit] = {
+    val sizeLimit = 5
+    for {
+      q <- InspectableQueue.bounded[IO, Unit](sizeLimit)
+
+      job = (jobId: Int) =>
+        Stream
+          .emit(42)
+          .covary[IO]
+          .evalMap(_ => q.enqueue1(()) *> putStrLn[IO](show"$jobId: offered to queue"))
+          .compile
+          .drain
+          .start
+
+      _ <- (1 to sizeLimit).toList.traverse(job).map(_.combineAll)
+      _ <- q.size.discrete.evalMap(size => putStrLn[IO](show"queue size: $size")).compile.drain.start
+      _ <- q.full.discrete.takeWhile(!_).compile.drain
+    } yield ()
+  }
 
   override def run(args: List[String]): IO[ExitCode] =
-    IO(println("hello world!"))
+    hangingQueue
       .as(ExitCode.Success)
 }
