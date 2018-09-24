c452a75a6bbb4eb360fb34b2bc685ef187d45189
Jakub Kozłowski <kubukoz@gmail.com>
2018-09-24 12:15:46 +0200

    Add explanatory comments, minimize sample

--- src/main/scala/io/typelevel/Playground.scala
+++ src/main/scala/io/typelevel/Playground.scala
@@ -2,8 +2,10 @@ package io.typelevel
 import cats.effect._
 import cats.implicits._
 import fs2.concurrent.InspectableQueue
+import fs2.Stream
 
 import scala.language.higherKinds
+import scala.concurrent.duration._
 
 object Playground extends IOApp {
 
@@ -14,13 +16,20 @@ object Playground extends IOApp {
   val hangingQueue: IO[Unit] = {
     val sizeLimit = 5
     for {
-      q <- InspectableQueue.bounded[IO, Unit](sizeLimit)
+      q <- InspectableQueue.unbounded[IO, Unit](sizeLimit)
 
-      job = (jobId: Int) =>
-        (q.enqueue1(()) *> putStrLn[IO](show"$jobId: offered to queue")).start
+      job = (jobId: Int) => q.enqueue1(()) *> putStrLn[IO](show"$jobId: offered to queue")
 
-      _ <- (1 to sizeLimit).toList.traverse(job).map(_.combineAll)
+      //enqueue elements concurrently - this should eventually make the queue full
+      _ <- (1 to sizeLimit).toList.parTraverse_(job).start
+
+      //show the latest queue's size after changes
       _ <- q.size.discrete.evalMap(size => putStrLn[IO](show"queue size: $size")).compile.drain.start
+
+      //show the queue's size every second
+      _ <- Stream.fixedRate[IO](1.second).evalMap(_ => q.size.get).map(_.show).evalMap(putStrLn[IO]).compile.drain.start
+
+      //terminate the program when the queue is full
       _ <- q.full.discrete.takeWhile(!_).compile.drain
     } yield ()
   }
